4f555ac38d3c4c4d5229dd43f722effb9d0afc99
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-06-29 00:08:43 +0900

    chore : 네브 메인 푸터 나누기

--- moma/src/App.jsx
+++ moma/src/App.jsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import styled from 'styled-components';
+import { Reset } from 'styled-reset';
+import { Outlet, Route, Routes, useLocation } from 'react-router-dom';
+import NavBar from './components/layout/NavBar';
+import Main from './pages/Main/main';
+import DetailManito from './pages/Detail/DetailManito';
+import CreateManito from './pages/Create/CreateManito';
+import AuthLogin from './pages/Auth/AuthLogin';
+import AuthSingUp from './pages/Auth/AuthSingUp';
+import CreateDoneManito from './pages/Create/CreateDoneManito';
+import Footer from './components/layout/Footer';
 
+
+
+
+const BodyWrapper = styled.div`
+text-align: center;
+margin-top: 90px;
+margin-bottom: 150px;
+padding: 10px;
+// felx-grow: 1;
+`;
+
+const LayoutWrapper = styled.div`
+/* display: flex;
+flex-direction: column;
+min-height: 100vh; */
+
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: #F5F5F5;
+  
+`;
+
+const ContentWrapper = styled.div`
+flex-grow: 1;
+`
+
+
+
+const Layout = () => {
+const location = useLocation();
+
+return (
+  <>
+    <LayoutWrapper>
+      <ContentWrapper>
+      <NavBar />
+        <BodyWrapper>
+          {/* <TransitionGroup>
+            <CSSTransition key={location.key} classNames="fade" timeout={300}> */}
+              <Outlet />
+            {/* </CSSTransition>
+          </TransitionGroup> */}
+        </BodyWrapper>
+      </ContentWrapper>
+      <Footer />
+    </LayoutWrapper>
+  </>
+);
+};
+
+// 로그인 여부 파악하기
+// 만약 로그인 유저시 아래 Routes보이게 하기
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+   <Reset/>
+          {/* 로그인 및 회원 가입 */}
+          <Routes>
+                <Route path="/login" element={<AuthLogin/>}/>
+                <Route path="/signup" element={<AuthSingUp/>}/>
+          
+                  <Route element={<Layout />}>
+                  <Route path="" index element={<Main />} />
+                  <Route path="/detail/:id" element={<DetailManito />} />
+                  <Route path="/create" element={<CreateManito />} />
+
+                  {/* 완료 마니또 전달 */}
+                  <Route path="/createDone" element={<CreateDoneManito/>}/>                  
+                  {/* <Route path="*" element={<NotFound/>} /> */}
+              </Route>
+          </Routes>
+          
     </>
-  )
+  );
 }
 
 export default App
---
+++ moma/src/pages/Auth/AuthLogin.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AuthLogin() {
+  return (
+    <div>Login</div>
+  )
+}
