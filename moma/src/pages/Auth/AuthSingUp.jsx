7228c0de0ab4c686292b51b5f3374933b2ae881d
qqaazz0222 <qqaazz0222@naver.com>
2023-06-29 09:53:14 +0900

    reset

--- moma/src/pages/Auth/AuthSingUp.jsx
+++ moma/src/pages/Auth/AuthSingUp.jsx
@@ -1,17 +1,6 @@
-import React from 'react'
-import SignUpHeaderImg from "../../assets/images/SignUpImg.png"
-import { SignUpHeaderImgBox, SignupWrapper } from './style'
-
+import React from "react";
+import { SignupWrapper } from "./style";
 
 export default function AuthSingUp() {
-  return (
-    
-    <SignupWrapper>
-    
-      <SignUpHeaderImgBox src={SignUpHeaderImg} alt="SignUpHeaderImg" />
-
-
-    
-    </SignupWrapper>
-  )
+    return <SignupWrapper />;
 }
